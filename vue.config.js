7a4d3da52a2b540de88677ad635fc20fc4d597f1
dragonjet <jetriconew@gmail.com>
2021-02-12 00:51:12 -0800

    Webpack og meta, npm packages, readme clear

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,23 @@ const path = require('path');
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/schedule.prismproject.jp/' : '',
   outputDir: path.resolve(__dirname, 'docs'),
+  lintOnSave: false,
   transpileDependencies: [
     'vuetify',
   ],
+  chainWebpack: (config) => {
+    // HTML Title used for OpenGraph linters (social media links)
+    config.plugin('html').tap((args) => {
+      // eslint-disable-next-line no-param-reassign
+      args[0].title = 'Prism Project Schedule';
+      // eslint-disable-next-line no-param-reassign
+      args[0].meta = {
+        'og:title': 'Prism Project Schedule',
+        'og:type': 'website',
+        'og:description': 'PRISM project is a brand new multinational vTuber agency to melt boundaries around the world.',
+        'og:image': 'https://vtubertools.github.io/schedule.prismproject.jp/img/icons/android-chrome-512x512.png',
+      };
+      return args;
+    });
+  },
 };
