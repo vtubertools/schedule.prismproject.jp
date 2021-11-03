46729edd01ab9225fa1b3c3225d9927a17011bad
dragonjet <jetriconew@gmail.com>
2021-11-03 03:27:14 -0700

    Add fourth gen

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -225,6 +225,30 @@
         <div class="pt-4"></div>
         <v-divider v-show="drawer"></v-divider>
 
+        <div class="section-header section-menu subtitle-2 grey--text pt-4" v-show="drawer">
+          FOURTH GENERATION
+        </div>
+        <v-expand-transition>
+          <v-list v-show="drawer">
+            <v-list-item
+              :class="'stream-entry pr-1 py-0'.split(' ').concat([ drawer ? 'pl-2' : 'pl-0' ])"
+              v-for="channel in displayChannels4" :key="channel.id"
+              @click="goChannel(channel.twitter)">
+              <v-list-item-avatar class="my-0">
+                <img :src="channel.image" referrerPolicy="no-referrer">
+              </v-list-item-avatar>
+              <v-list-item-content class="pt-2 pb-2">
+                <v-list-item-title>{{channel.name}}</v-list-item-title>
+                <!-- <v-list-item-subtitle>
+                  {{displayNumber(channel.subscribers)}} subscribers
+                </v-list-item-subtitle> -->
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-expand-transition>
+        <div class="pt-4"></div>
+        <v-divider v-show="drawer"></v-divider>
+
         <!-- TOGGLE DARK MODE -->
         <v-list>
           <v-list-item class="stream-entry pr-1 px-0 py-0 pl-2" @click="toggleDarkMode()">
@@ -255,6 +279,7 @@ export default {
     displayChannels1: [],
     displayChannels2: [],
     displayChannels3: [],
+    displayChannels4: [],
     showMenu: false,
     gens: {
       UC2hWFlqMew61Jy6A8zu5HzQ: 1,
@@ -266,6 +291,11 @@ export default {
       UCw1KNjVqfrJSfcFd6zlcSzA: 3,
       'UCpeRj9-GaLGNUoKdI5I7vZA': 3,
       UC0ZTVxCHkZanT5dnP2FZD4Q: 3,
+
+      UCgM1sly6rnxqWe8GLbceEag: 4,
+      'UCx_A6fns9qKjybu-6k0ur1g': 4,
+      UCkJ64W0J7R0zCSAsZfRIhVQ: 4,
+      'UCKRGwowORbvAnoeZ-G06duQ': 4,
     },
   }),
   watch: {
@@ -301,6 +331,11 @@ export default {
         return 0;
       });
       this.displayChannels3 = this.displayChannels3.sort((a, b) => {
+        if (a.published > b.published) return 1;
+        if (a.published < b.published) return -1;
+        return 0;
+      });
+      this.displayChannels4 = this.displayChannels4.sort((a, b) => {
         if (a.published > b.published) return 1;
         if (a.published < b.published) return -1;
         return 0;
